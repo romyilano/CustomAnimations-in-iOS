8f19e3da643e505af97f91180633c1565f2a99b3
Romy Ilano <romy@snowyla.com>
2015-09-08 16:54:20 -0700

    layer review

--- CustomStoryboardSegue/ViewController.m
+++ CustomStoryboardSegue/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+@import QuartzCore;
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *layerView;
 
 @end
 
@@ -17,6 +19,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    /* 
+     From advanced Core Animation - this stuff is really easy to forget!
+     layer lacks the responder chain, autolayout capabilities, etc. 
+     and the cost to add a UIView instead of a layer is minimal
+     */
+    CALayer *blueLayer = [CALayer layer];
+    blueLayer.frame = CGRectMake(50.0f, 50.0f, 100.0f, 100.0f);
+    // easy to forget if you don't type this every day =D
+    blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    
+    // easy to forget - every UIView has a CALayer backing layer (here the layer property)
+    [self.layerView.layer addSublayer:blueLayer];
 }
 
 - (void)didReceiveMemoryWarning {
