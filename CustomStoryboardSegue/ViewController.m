117265d3deb8b5af5b5174a5be07e141e48f46fc
Romy Ilano <romy@snowyla.com>
2015-09-08 17:03:28 -0700

    messing with the contents property

--- CustomStoryboardSegue/ViewController.m
+++ CustomStoryboardSegue/ViewController.m
@@ -30,6 +30,21 @@
     // easy to forget if you don't type this every day =D
     blueLayer.backgroundColor = [UIColor blueColor].CGColor;
     
+    UIImage *sudoRoomImage = [UIImage imageNamed:@"SudoRoom.jpg"];
+    CALayer *imageLayer = [CALayer layer];
+    imageLayer.frame = CGRectMake(0.0f, 0.0f, 50.0f, 50.0f);
+    imageLayer.backgroundColor = [UIColor redColor].CGColor;
+    
+    /*
+     contents is a quirky one - you have to bridge it if you're using ARC
+     /* An object providing the contents of the layer, typically a CGImageRef,
+     * but may be something else. (For example, NSImage objects are
+     * supported on Mac OS X 10.6 and later.) Default value is nil.
+     * Animatable. */
+ 
+    imageLayer.contents = (__bridge id)sudoRoomImage.CGImage;
+
+    [self.layerView.layer addSublayer:imageLayer];
     // easy to forget - every UIView has a CALayer backing layer (here the layer property)
     [self.layerView.layer addSublayer:blueLayer];
 }
